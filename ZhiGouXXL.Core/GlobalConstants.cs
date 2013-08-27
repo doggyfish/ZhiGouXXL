4031217005738cefdbe07f635e29e7fae96abdf8
Wei Yu <fff_yu@hotmail.com>
2013-08-27 19:35:22 +1200

    Initial Commit

---
+++ ZhiGouXXL.Core/GlobalConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZhiGouXXL.Core {
+	public class GlobalConstants {
+		#region const strings
+
+		public const string UserName = "用户名";
+		public const string Password = "密码";
+		public const string ConfirmPassword = "确认密码";
+		public const string Address = "收件地址";
+		public const string Mobile = "联系电话";
+
+		#endregion
+	}
+}
---
+++ ZhiGouXXL.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZhiGouXXL.Web.MvcApplication" Language="C#" %>
